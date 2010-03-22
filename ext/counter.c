acb459035f1f7b6e392d77716af78c3f0fbabafe
Peter Brown <peter@frankenstein.(none)>
2010-03-22 22:59:01 +0000

    Defined a 'next' method.  Test failing.
    We have defined a method on the Counter class which does nothing.
    
    The 'rb_define_method' associates the Ruby string (name of method)
    with the C function.  As C is not an object-oriented language, all
    the functions (in a Ruby extension) take the object on which they
    act as the first parameter.  The fourth parameter of '0' indicates
    that the (Ruby) method takes no parameters.  Thus the C function
    has one parameter 'self' (the object associated with the call).
    The type of the values coming in from and returning to the Ruby
    environment are 'VALUE'.  More on this later.
    
    All (Ruby) methods must return a value (unlike C functions): if
    nothing else, a nil.  Produced in C with 'Qnil'.

--- ext/counter.c
+++ ext/counter.c
@@ -1,5 +1,12 @@
 #include "ruby.h"
 
+static VALUE next( VALUE self ) {
+    return Qnil;
+}
+
+static VALUE counter;
+
 void Init_counter() {
-    rb_define_class( "Counter", rb_cObject );
+    counter = rb_define_class( "Counter", rb_cObject );
+    rb_define_method( counter, "next", next, 0 );
 }

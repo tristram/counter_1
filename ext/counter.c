ece2ed3dbb3753793690f944fcef89c82504ac7d
Peter Brown <peter@frankenstein.(none)>
2010-03-23 09:38:13 +0000

    Allocate a structure to hold the state.
    We define a structure (currently empty) to hold the state of the
    Counter object.  I have called the C function which allocates the
    memory for this 'allocate_structure'.  It is tied to the Ruby
    infrastructure with the 'rb_define_alloc_func' call.  Having
    allocated the structure, it is wrapped so Ruby can handle it with
    the 'Data_Wrap_Struct' call.  This takes four parameters.  The
    class_name is passed in because we may have been subclassed.  A
    mark routine would be required if our data held references to Ruby
    objects.  (We won't get into that.)

--- ext/counter.c
+++ ext/counter.c
@@ -1,17 +1,35 @@
 #include "ruby.h"
 
-static VALUE next( VALUE self ) {
-    return INT2FIX( 1 );
+typedef struct {
+} Counter;
+
+static void free_structure( Counter* cp ) {
+    free( cp );
+}
+
+static VALUE allocate_structure( VALUE class_name ) {
+    Counter* cp = calloc( 1, sizeof(Counter) );
+    return Data_Wrap_Struct(
+            class_name,         // class owning this memory
+            0,                  // no mark routine required
+            free_structure,     // function called when garbage collected
+            cp
+    );
 }
 
 static VALUE initialize( VALUE self ) {
     return self;
 }
 
+static VALUE next( VALUE self ) {
+    return INT2FIX( 1 );
+}
+
 static VALUE counter;
 
 void Init_counter() {
     counter = rb_define_class( "Counter", rb_cObject );
+    rb_define_alloc_func( counter, allocate_structure );
     rb_define_method( counter, "initialize", initialize, 0 );
     rb_define_method( counter, "next", next, 0 );
 }

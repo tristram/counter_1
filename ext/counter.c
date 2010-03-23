538d74e00f6d61863a71f178d81405a764cedd79
Peter Brown <peter@frankenstein.(none)>
2010-03-23 09:14:04 +0000

    Added an initialize method.
    We need an initialize method in the C code to have somewhere we
    can allocate the memory which will hold the class's state.
    
    'initialize' is a method like any other, and needs to return a
    VALUE.  Traditionally (ie in the Pickaxe) you return 'self', but
    'Qnil' seems to work as well.

--- ext/counter.c
+++ ext/counter.c
@@ -4,9 +4,14 @@ static VALUE next( VALUE self ) {
     return INT2FIX( 1 );
 }
 
+static VALUE initialize( VALUE self ) {
+    return self;
+}
+
 static VALUE counter;
 
 void Init_counter() {
     counter = rb_define_class( "Counter", rb_cObject );
+    rb_define_method( counter, "initialize", initialize, 0 );
     rb_define_method( counter, "next", next, 0 );
 }
